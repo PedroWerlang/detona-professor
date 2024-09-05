38167bfab98511c471d243822fa6964c07b25377
PedroWerlang <130494691+PedroWerlang@users.noreply.github.com>
2024-09-04 21:06:59 -0400

    Update ProfessorDAOImp.java
    Atualização

--- src/main/java/br/ufmt/alg3/professor/ProfessorDAOImp.java
+++ src/main/java/br/ufmt/alg3/professor/ProfessorDAOImp.java
@@ -1,33 +1,23 @@
 package br.ufmt.alg3.professor;
 
-import com.sun.tools.javac.util.List;
+import java.util.List;
 
-import br.ufmt.alg3.curso.Curso;
+public class ProfessorDAOImpl implements ProfessorDAO {
 
-public class ProfessorDAOImp implements ProfessorDAO {
-
-private List<Curso> banco = new ArrayList<Curso>();
-
-    public void inserir(Curso curso){
-        System.out.println("Inserindo curso");
-        banco.add(curso);
-    } 
-
-    public void remover(Curso curso){
-        System.out.println("Removendo Curso");
-        banco.remove(curso);
-    }
-        
-
-    public void atualizar(Curso curso){
-        System.out.println("Atualizando curso");
-        int idx = banco.indexOf(curso);
-        if(idx != -1){
-            banco.set(idx, curso)
-        }
+    public void inserir(Professor professor) {
+        System.out.println("Inserindo professor");
     }
 
-    public List<Curso> leitura(){
-        return banco;
+    public void remover(Professor professor) {
+        System.out.println("Removendo professor");
     }
+
+    public void atualizar(Professor professor) {
+        System.out.println("Atualizando professor");
+    }
+
+    public List<Professor> leitura() {
+        return null;
+    }
+    
 }
