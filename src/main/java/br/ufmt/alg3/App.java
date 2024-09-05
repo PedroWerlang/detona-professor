a8862e117fb0a54f325961192cdecb99e266e894
PedroWerlang <130494691+PedroWerlang@users.noreply.github.com>
2024-09-04 21:07:21 -0400

    Update App.java
    Atualização

--- src/main/java/br/ufmt/alg3/App.java
+++ src/main/java/br/ufmt/alg3/App.java
@@ -1,27 +1,35 @@
 package br.ufmt.alg3;
 
 import java.util.Scanner;
+
 import br.ufmt.alg3.curso.Curso;
+import br.ufmt.alg3.curso.CursoDAO;
+import br.ufmt.alg3.curso.CursoDAOImpl;
 import br.ufmt.alg3.professor.Professor;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        Curso[] respostas;
-        respostas = new Curso[3];
-        Scanner teclado;
-        teclado = new Scanner(System.in);
-        for(int i = 0; i < respostas.length; i++){
-            Curso c = new Curso();
-            System.out.print("Digite o nome do curso: ");
-            String nome = teclado.nextLine();
-            c.setNome(nome);
-            System.out.println("O nome digitado foi: " + c.getNome());
-            respostas[i] = c;
-        }
-        for(Curso curso :respostas){
-            System.out.println("Curso avaliado: " + curso.getNome());
-        }
+        // https://github.com/thesivis/detona-professor
+        Curso c = new Curso();
+        c.setNome("SI");
+        CursoDAO banco = new CursoDAOImpl();
+        banco.inserir(c);
+    }
+
+    public static void imprimir(Identificador i, Professor p){
+        System.out.println("Identificador Duplo");
+        System.out.println(i.imprimir());
+    }
+
+    public static void imprimir(Identificador i){
+        System.out.println("Identificador");
+        System.out.println(i.imprimir());
+    }
+
+    public static void imprimir(Professor i){
+        System.out.println("Professor");
+        System.out.println(i.imprimir());
     }
 }
