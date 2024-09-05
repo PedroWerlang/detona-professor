37100d4c06ec4e146e80e541eb74d6d95ad53832
PedroWerlang <130494691+PedroWerlang@users.noreply.github.com>
2024-09-04 21:06:15 -0400

    Update ProfessorDAO.java
    Atualização

--- src/main/java/br/ufmt/alg3/professor/ProfessorDAO.java
+++ src/main/java/br/ufmt/alg3/professor/ProfessorDAO.java
@@ -1,5 +1,14 @@
 package br.ufmt.alg3.professor;
 
-public class ProfessorDAO {
+import java.util.List;
+
+public interface ProfessorDAO {
     
+    public void inserir(Professor professor);
+
+    public void remover(Professor professor);
+
+    public void atualizar(Professor professor);
+
+    public List<Professor> leitura();
 }
