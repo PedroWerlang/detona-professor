f470873db3593075c243bf872def4b6e6e3f9771
PedroWerlang <130494691+PedroWerlang@users.noreply.github.com>
2024-10-16 18:46:13 -0400

    Update CursoDAOImp.java

--- src/main/java/br/ufmt/alg3/curso/CursoDAOImp.java
+++ src/main/java/br/ufmt/alg3/curso/CursoDAOImp.java
@@ -3,13 +3,20 @@ package br.ufmt.alg3.curso;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.ufmt.alg3.Utils;
+
 public class CursoDAOImpl implements CursoDAO {
 
     private List<Curso> banco = new ArrayList<Curso>();
     
     public void inserir(Curso curso){
         System.out.println("Inserindo curso");
-        banco.add(curso);
+        try {
+            curso.validar();
+            banco.add(curso);
+        } catch (Exception erro) {
+            System.out.println(erro.getMessage());
+        }
     }
 
     public void remover(Curso curso){
@@ -19,9 +26,14 @@ public class CursoDAOImpl implements CursoDAO {
 
     public void atualizar(Curso curso){
         System.out.println("Atualizando curso");
-        int idx = banco.indexOf(curso);
-        if (idx != -1) {
-            banco.set(idx, curso);
+        try {
+            curso.validar();
+            int idx = banco.indexOf(curso);
+            if (idx != Utils.NAO_ACHOU) {
+                banco.set(idx, curso);
+            }
+        } catch (Exception erro) {
+            System.out.println(erro.getMessage());
         }
     }
 
